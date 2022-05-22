7574aa6130a1ad945fa313f920a9fbc9dd1826a9
marosan28 <danijela.marosan36@gmail.com>
2022-05-22 13:43:48 +0000

    Add pyfiglet heading and function for words

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import pyfiglet
 from words import list_of_words
 
 # Defining variables
@@ -15,9 +16,33 @@ random_word = random.choice(list_of_words)
 
 
 def start():
+    heading = pyfiglet.figlet_format("HANGMAN")
+    print(heading)
     name = input("What is your name? ")
     print("Hello " + name + "!")
     print("Welcome to Hangman!")
     print("Can you guess the secret word?")
     print("You have 6 tries, best of luck!")
 
+
+def pick_word():
+    return random_word.lower()
+
+
+def guessed_word():
+    global right_characters
+    for x in range(0, len(random_word)):
+        character = random_word[x]
+        if character in right_characters:
+            print(character, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+
+def main():
+    start()
+
+
+if __name__ == '__main__':
+    main()
