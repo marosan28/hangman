5316d8609a83f015fa0df51f65691d5793f86c95
marosan28 <danijela.marosan36@gmail.com>
2022-05-22 13:18:01 +0000

    Add starting function

--- run.py
+++ run.py
@@ -8,4 +8,16 @@ right_characters = []
 wrong_characters = []
 tries = 6
 game = False
+guessed = False
 random_word = random.choice(list_of_words)
+
+# Start function
+
+
+def start():
+    name = input("What is your name? ")
+    print("Hello " + name + "!")
+    print("Welcome to Hangman!")
+    print("Can you guess the secret word?")
+    print("You have 6 tries, best of luck!")
+
